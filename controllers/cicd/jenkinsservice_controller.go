6b7f8f743032fc89db0679d5d90f9e90af6ab645
Jacky <jacky01.zhang@outlook.com>
2023-10-19 18:46:51 +0800

    finish controller

--- controllers/cicd/jenkinsservice_controller.go
+++ controllers/cicd/jenkinsservice_controller.go
@@ -19,7 +19,13 @@ package cicd
 import (
 	"context"
 
+	apps "k8s.io/api/apps/v1"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/uuid"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -33,7 +39,7 @@ type JenkinsServiceReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=cicd,resources=jenkinsservices,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=cicd,resources=jenkinsservices,verbs=create;delete
 //+kubebuilder:rbac:groups=cicd,resources=jenkinsservices/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=cicd,resources=jenkinsservices/finalizers,verbs=update
 
@@ -47,9 +53,67 @@ type JenkinsServiceReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *JenkinsServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
+	logger.Info("reconcile is triggered. ")
 
-	// TODO(user): your logic here
+	js := &cicdv1.JenkinsService{}
+	err := r.Get(ctx, req.NamespacedName, js)
+	// 失败了直接返回，垃圾收集会清理js
+	if errors.IsNotFound(err) {
+		logger.Info("The Jenkins Service has been deleted")
+		return ctrl.Result{}, nil
+	}
+	// 有错误会使得js再入工作队列，不用我们关心
+	if err != nil {
+		logger.Error(err, "Get jenkins service failed")
+		return ctrl.Result{}, err
+	}
+
+	var replicas int32
+	replicas = int32(js.Spec.InstanceAmount)
+	selector := map[string]string{}
+	selector["type"] = "jenkinsservice"
+	selector["jsname"] = js.Name
+
+	// 创建一个Deployment
+	d := apps.Deployment{
+		TypeMeta: metav1.TypeMeta{APIVersion: "apps/v1", Kind: "Deployment"},
+		ObjectMeta: metav1.ObjectMeta{
+			UID:         uuid.NewUUID(),
+			Name:        js.Name,
+			Namespace:   js.Namespace,
+			Annotations: make(map[string]string),
+		},
+		Spec: apps.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{MatchLabels: selector},
+			Template: core.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: selector,
+				},
+				Spec: core.PodSpec{
+					Containers: []core.Container{
+						{
+							Name:            "jenkinsforjs",
+							Image:           "nginx:latest",
+							ImagePullPolicy: "IfNotPresent",
+						},
+					},
+				},
+			},
+		},
+	}
+	err = r.Get(ctx, types.NamespacedName{Namespace: js.Namespace, Name: js.Name}, &apps.Deployment{})
+	if err == nil {
+		logger.Info("Deployment with the same name is existed already")
+		return ctrl.Result{}, nil
+	}
+
+	err = r.Create(ctx, &d, &client.CreateOptions{})
+	if err != nil {
+		logger.Error(err, "create deployment failed")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
